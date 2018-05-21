0970c047efa720a17bae15c3a1e51c64c8014569
WaryasJ85 <37874309+WaryasJ85@users.noreply.github.com>
2018-05-21 16:44:33 -0400

    05212018
    Finished button pushes test case started Adding event test case.

---
+++ US Lead Gen Reporting Tool/US Lead Gen Reporting Tool/AddAnEvent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using Selenium.Helper;
+using Selenium.WebDriver;
+using Selenium.WebForms;
+
+
+
+namespace US_Lead_Gen_Reporting_Tool
+{
+    [TestClass]
+    public class AddAnEvent
+    {
+        [TestMethod]
+        public void Use_This_Test_To_Add_An_Event()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                
+                //This is for mouseover actions.
+                Actions builder = new Actions(driver);
+
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[1]/span")).Perform();
+
+                Thread.Sleep(2000);
+
+                builder.MoveToElement(driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-highlight.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last")).Perform();
+
+                Thread.Sleep(2000);
+
+                //Selects the Source Codes Button in the system dropdown 
+                driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last.k-state-border-right > div > ul > li:nth-child(2)").Click();
+
+                Thread.Sleep(1000);
+
+
+            }
+        }
+    }
+}
--- US Lead Gen Reporting Tool/US Lead Gen Reporting Tool/PushingSomeButtons.cs
+++ US Lead Gen Reporting Tool/US Lead Gen Reporting Tool/PushingSomeButtons.cs
@@ -9,6 +9,7 @@ using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using Selenium.Helper;
 using Selenium.WebDriver;
+using Selenium.WebForms;
 
 namespace US_Lead_Gen_Reporting_Tool
 {
@@ -47,7 +48,7 @@ namespace US_Lead_Gen_Reporting_Tool
                 //Thread.Sleep(2500);
 
                 builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[1]/span")).Perform();
-                
+
                 //Stops the driver so the dropdown can load. 
                 Thread.Sleep(1000);
 
@@ -65,7 +66,7 @@ namespace US_Lead_Gen_Reporting_Tool
                 driver.FindElementByXPath("//*[@id='tabstrip']/ul/li[3]/a").Click();
 
                 Thread.Sleep(1500);
-                
+
                 //Incoming Held Leads Tab
                 driver.FindElementByXPath("//*[@id='tabstrip']/ul/li[1]/a").Click();
 
@@ -82,6 +83,18 @@ namespace US_Lead_Gen_Reporting_Tool
                 Thread.Sleep(2000);
 
                 //In the future i would like to put an if statement in here for further testing.
+
+                //if (true)
+                //{
+                //    bla bla bla put if ture here
+                //}
+
+                //else
+                //{
+
+                //    If false go there
+
+                //}
 
                 driver.FindElementByXPath("//*[@id='NavigationMenu']/li[2]").Click();
 
@@ -112,14 +125,159 @@ namespace US_Lead_Gen_Reporting_Tool
                 //Zipcodes button
                 driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-default.k-first > div > ul > li:nth-child(4)").Click();
 
-                Thread.Sleep(5000);
+                Thread.Sleep(1000);
 
                 builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[1]/span")).Perform();
 
+                Thread.Sleep(1000);
 
+                //span the system tab
+                builder.MoveToElement(driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-highlight.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last")).Perform();
 
+                Thread.Sleep(2000);
 
+                //Selects the Events button in the dropdown
+                driver.FindElementByLinkText("Events").Click();
 
+                Thread.Sleep(2000);
+
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[1]/span")).Perform();
+
+                Thread.Sleep(2000);
+
+                builder.MoveToElement(driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-highlight.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last")).Perform();
+
+                Thread.Sleep(2000);
+
+                //Selects the Source Codes Button in the system dropdown 
+                driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last.k-state-border-right > div > ul > li:nth-child(2)").Click();
+
+                Thread.Sleep(1000);
+
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[1]/span")).Perform();
+
+                Thread.Sleep(1000);
+
+                //span the system tab
+                builder.MoveToElement(driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-highlight.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last")).Perform();
+
+                Thread.Sleep(2000);
+
+                //Selects the System Settings Button in the system dropdown
+                driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last.k-state-border-right > div > ul > li:nth-child(3)").Click();
+
+                Thread.Sleep(1000);
+
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[1]/span")).Perform();
+
+                Thread.Sleep(2000);
+
+                builder.MoveToElement(driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-highlight.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last")).Perform();
+
+                Thread.Sleep(2000);
+
+                //Selects the Plug-in Management in the system dropdown
+                driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last.k-state-border-right > div > ul > li:nth-child(4)").Click();
+
+                Thread.Sleep(1000);
+
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[1]/span")).Perform();
+
+                Thread.Sleep(1000);
+
+                //span the system tab
+                builder.MoveToElement(driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-highlight.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last")).Perform();
+
+                Thread.Sleep(2000);
+
+                //Selects the Exporter Settings button in the system dropdown
+                driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last.k-state-border-right > div > ul > li:nth-child(5)").Click();
+
+                Thread.Sleep(1000);
+
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[1]/span")).Perform();
+
+                Thread.Sleep(1000);
+
+                //span the system tab
+                builder.MoveToElement(driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-highlight.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last")).Perform();
+
+                Thread.Sleep(2000);
+
+                //Selects the Provision Keys button in the system dropdown
+                driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last.k-state-border-right > div > ul > li:nth-child(6)").Click();
+
+                Thread.Sleep(1000);
+
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[1]/span")).Perform();
+
+                Thread.Sleep(1000);
+
+                //span the system tab
+                builder.MoveToElement(driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-highlight.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last")).Perform();
+
+                Thread.Sleep(2000);
+
+                //Selects The Purginator button in the system dropdown
+                driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last.k-state-border-right > div > ul > li:nth-child(7)").Click();
+
+                Thread.Sleep(1000);
+
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[1]/span")).Perform();
+
+                Thread.Sleep(1000);
+
+                //span the system tab
+                builder.MoveToElement(driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-highlight.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last")).Perform();
+
+                Thread.Sleep(2000);
+
+                //Selects the Purge History button in the system dropdown
+                driver.FindElementByCssSelector("#NavigationMenu > li.k-item.main-menu-item.k-state-default.k-first.k-state-border-down > div > ul > li.k-item.k-state-default.k-last.k-state-border-right > div > ul > li.k-item.k-state-default.k-last").Click();
+
+                Thread.Sleep(2000);
+
+                //Mouse hovers over the Reporting dropdown
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[3]/span")).Perform();
+
+                Thread.Sleep(3000);
+
+                //Selects the Daily eamil button in the reporting dropdown
+                driver.FindElementByLinkText("Daily Emails").Click();
+
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[3]/span")).Perform();
+
+                Thread.Sleep(2000);
+                
+                //Selects the EDC Lead Summary from the Reporting dropdown
+                driver.FindElementByLinkText("EDC Lead Summary").Click();
+
+                Thread.Sleep(200);
+
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[3]/span")).Perform();
+
+                Thread.Sleep(2000);
+
+                //Selects the Transmitted Leads from the Reporting dropdown
+                driver.FindElementByLinkText("Transmitted Leads").Click();
+
+                Thread.Sleep(2000);
+
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[3]/span")).Perform();
+
+                Thread.Sleep(2000);
+
+                driver.FindElementByLinkText("Lead Details").Click();
+
+                Thread.Sleep(200);
+
+                builder.MoveToElement(driver.FindElementByXPath("//*[@id='NavigationMenu']/li[3]/span")).Perform();
+
+                Thread.Sleep(2000);
+
+                driver.FindElementByLinkText("Multi-Brand Details").Click();
+
+                Thread.Sleep(5000);
 
 
 
